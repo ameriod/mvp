adf8b80da88d62b2680af06d22057e164c45ec56
Parker <ameriod@gmail.com>
2016-10-29 14:46:44 -0400

    first commit, created empty project

---
+++ app/src/main/java/me/ameriod/lib/mvp/app/MainActivity.java
@@ -0,0 +1,13 @@
+package me.ameriod.lib.mvp.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
